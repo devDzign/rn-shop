b4b7fe482162f33e90d157cb57b77d8c165d45e1
devDzign <chabour.mourad87@gmail.com>
2020-11-06 22:28:25 +0100

    init start project with redux

---
+++ src/constants/color.js
@@ -0,0 +1,6 @@
+export default {
+    primaryColor: '#000',
+    primaryColorOld: '#4a148c',
+    accentColor: '#ff6f00',
+    white: '#FFF'
+}
---
+++ src/store/store.js
@@ -0,0 +1,21 @@
+import {createStore, applyMiddleware, compose} from "redux";
+
+// middlewares
+import thunk from "redux-thunk";
+import logger from 'redux-logger';
+import rootReducer from './reducer-root';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+const middlewares = [thunk, logger];
+
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = composeWithDevTools({
+    // Specify name here, actionsBlacklist, actionsCreators and other options if needed
+});
+
+export const store = createStore(
+    rootReducer,
+    composeEnhancers(
+        applyMiddleware(...middlewares)
+    )
+);
